17c175c258111f3789ce9c1ce166890dd0ce00ab
JoramWilander <jwawilander@gmail.com>
2016-08-18 08:54:57 -0400

    Add notification settings to control what statuses allow push notifications

--- api/post.go
+++ api/post.go
@@ -587,7 +587,7 @@ func sendNotifications(c *Context, post *model.Post, team *model.Team, channel *
 	}
 
 	mentionedUserIds := make(map[string]bool)
-	alwaysNotifyUserIds := []string{}
+	allActivityPushUserIds := []string{}
 	hereNotification := false
 	updateMentionChans := []store.StoreChannel{}
 
@@ -651,7 +651,7 @@ func sendNotifications(c *Context, post *model.Post, team *model.Team, channel *
 			if profile.NotifyProps["push"] == model.USER_NOTIFY_ALL &&
 				(post.UserId != profile.Id || post.Props["from_webhook"] == "true") &&
 				!post.IsSystemMessage() {
-				alwaysNotifyUserIds = append(alwaysNotifyUserIds, profile.Id)
+				allActivityPushUserIds = append(allActivityPushUserIds, profile.Id)
 			}
 		}
 	}
@@ -722,13 +722,28 @@ func sendNotifications(c *Context, post *model.Post, team *model.Team, channel *
 
 	if sendPushNotifications {
 		for _, id := range mentionedUsersList {
-			if profileMap[id].NotifyProps["push"] != "none" {
+			var status *model.Status
+			var err *model.AppError
+			if status, err = GetStatus(id); err != nil {
+				status = &model.Status{id, model.STATUS_OFFLINE, 0}
+			}
+
+			if profileMap[id].StatusAllowsPushNotification(status) {
 				sendPushNotification(post, profileMap[id], channel, senderName, true)
 			}
 		}
-		for _, id := range alwaysNotifyUserIds {
+
+		for _, id := range allActivityPushUserIds {
 			if _, ok := mentionedUserIds[id]; !ok {
-				sendPushNotification(post, profileMap[id], channel, senderName, false)
+				var status *model.Status
+				var err *model.AppError
+				if status, err = GetStatus(id); err != nil {
+					status = &model.Status{id, model.STATUS_OFFLINE, 0}
+				}
+
+				if profileMap[id].StatusAllowsPushNotification(status) {
+					sendPushNotification(post, profileMap[id], channel, senderName, false)
+				}
 			}
 		}
 	}
